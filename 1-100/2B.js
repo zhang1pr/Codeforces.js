bd5ce7ce9742e1ec99fee974e428186828a761e9
Chao Zhang <czhang@axonify.com>
2022-11-09 04:06:17 -0500

    Change 2B

--- 1-100/2B.js
+++ 1-100/2B.js
@@ -28,26 +28,26 @@ function getFactorCount(num, factor) {
   return cnt;
 }
 
-function solve(N, arr) {
-  function getDP(k) {
-    const dp = [...Array(N)].map(() => []);
-    for (let i = 0; i < N; i++)
-      for (let j = 0; j < N; j++)
-        dp[i][j] = getFactorCount(arr[i][j], k);
+function getDP(k, N, arr) {
+  const dp = [...Array(N)].map(() => []);
+  for (let i = 0; i < N; i++)
+    for (let j = 0; j < N; j++)
+      dp[i][j] = getFactorCount(arr[i][j], k);
 
-    for (let i = 1; i < N; i++) {
-      dp[i][0] += dp[i-1][0];
-      dp[0][i] += dp[0][i-1];
-    }
-
-    for (let i = 1; i < N; i++)
-      for (let j = 1; j < N; j++)
-        dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
-            
-    return dp;
+  for (let i = 1; i < N; i++) {
+    dp[i][0] += dp[i-1][0];
+    dp[0][i] += dp[0][i-1];
   }
- 
-  let [two, five] = [getDP(2), getDP(5)]
+
+  for (let i = 1; i < N; i++)
+    for (let j = 1; j < N; j++)
+      dp[i][j] += Math.min(dp[i-1][j], dp[i][j-1]);
+          
+  return dp;
+}
+
+function solve(N, arr) { 
+  let [two, five] = [getDP(2, N, arr), getDP(5, N, arr)]
   let mat = two[N-1][N-1] < five[N-1][N-1] ? two : five;
   let res = mat[N-1][N-1];
 
