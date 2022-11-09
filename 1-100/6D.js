71e70fcd5324ad2009debe2f9724782915fcefab
Chao Zhang <czhang@axonify.com>
2022-11-09 01:08:20 -0500

    Change 6D

--- 1-100/6D.js
+++ 1-100/6D.js
@@ -17,55 +17,55 @@ console.log(hits);
 console.log(str);   
 
 function solve(N,A,B,arr) {
-	for (let i=0; i<N; i++) 
+  for (let i=0; i<N; i++) 
     arr[i]++;
 
-	let maxHP = Math.max(...arr);
-	let dp = [...Array(N+1)].map(
-		() => [...Array(maxHP+1)].map(
-			() => Array(maxHP+1).fill(Infinity)
-		) 
+  let maxHP = Math.max(...arr);
+  let dp = [...Array(N+1)].map(
+    () => [...Array(maxHP+1)].map(
+      () => Array(maxHP+1).fill(Infinity)
+    ) 
   );
 
-	let pre = dp.map(a=>a.map(b=>b.slice()));
+  let pre = dp.map(a=>a.map(b=>b.slice()));
   let next = dp.map(a=>a.map(b=>b.slice()));
 
-	dp[2][arr[0]][arr[1]] = 0;
+  dp[2][arr[0]][arr[1]] = 0;
 
-	for (let i = 2; i < N; i++) {
-		for (let j = 0; j <= arr[i - 2]; j++) {
-			for (let k = 0; k <= arr[i - 1]; k++) {
-				if (dp[i][j][k] == Infinity) continue;
+  for (let i = 2; i < N; i++) {
+    for (let j = 0; j <= arr[i - 2]; j++) {
+      for (let k = 0; k <= arr[i - 1]; k++) {
+        if (dp[i][j][k] == Infinity) continue;
         
-		    let min = Math.ceil(j / B);
-				let max = Math.max(min, Math.ceil(k / A), Math.ceil(arr[i] / B));
-				
-				for (let hit = min; hit <= max; hit++) {
-					let nj = Math.max(0, k - A * hit);
+        let min = Math.ceil(j / B);
+        let max = Math.max(min, Math.ceil(k / A), Math.ceil(arr[i] / B));
+        
+        for (let hit = min; hit <= max; hit++) {
+          let nj = Math.max(0, k - A * hit);
           let nk = Math.max(0, arr[i] - B * hit);
 
-					if (dp[i + 1][nj][nk] > dp[i][j][k] + hit) {
-						dp[i + 1][nj][nk] = dp[i][j][k] + hit;
-						pre[i + 1][nj][nk] = j;
-						next[i + 1][nj][nk] = k;
-					}
-				}
-			}
-		}
-	}
+          if (dp[i + 1][nj][nk] > dp[i][j][k] + hit) {
+            dp[i + 1][nj][nk] = dp[i][j][k] + hit;
+            pre[i + 1][nj][nk] = j;
+            next[i + 1][nj][nk] = k;
+          }
+        }
+      }
+    }
+  }
 
-	let res = [], j=0, k=0;
-	for (let i=N; i>=3; i--) {
-		let nj = pre[i][j][k], nk = next[i][j][k];
-		let hit = dp[i][j][k] - dp[i - 1][nj][nk];
-		j = nj;
+  let res = [], j=0, k=0;
+  for (let i=N; i>=3; i--) {
+    let nj = pre[i][j][k], nk = next[i][j][k];
+    let hit = dp[i][j][k] - dp[i - 1][nj][nk];
+    j = nj;
     k = nk;
 
-		while (hit > 0) {
+    while (hit > 0) {
       hit--;
       res.push(i-1);
     }
-	}
+  }
 
   return [dp[N][0][0], res.reverse().join(' ')];
 }
