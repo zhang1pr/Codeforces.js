cee7f7685ca72baae42b39ba2f6a52cec2b9e816
Chao Zhang <czhang@axonify.com>
2022-12-28 16:06:31 -0500

    Change 3D

--- 1-100/3D.js
+++ 1-100/3D.js
@@ -1,7 +1,7 @@
 // Heap
 // Take '?' as ')'
-// Keep a min heap of lCost - rCost turning ')' into '(' 
-// Poll the min when no '(' can match the current ')' and turn '(' into ')'
+// Keep a min heap of lCost - rCost as the upcoming cost changing ')' to '(' 
+// Poll the min from heap when no '(' can match the current ')' and change ')' to '('
 
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8').trim().split(/[\n\r]+/);
