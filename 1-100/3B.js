a0a2436adb8164d15379a65ceb9230c08096bce5
Chao Zhang <czhang@axonify.com>
2023-02-23 21:14:55 -0500

    Change 3B

--- 1-100/3B.js
+++ 1-100/3B.js
@@ -9,7 +9,7 @@ let count = 0;
 const readnum = () => input[count++].split(' ').map(a => +a);
 const readword = () => input[count++].split(' ');
 
-let [N,V] = readnum();
+let [N, V] = readnum();
 let arr = [];
 for (let i = 1; i <= N; i++)
   arr.push(readnum());
@@ -20,20 +20,20 @@ console.log(vehicle);
 
 function solve(arr, V) {
   let kayak = [], catamaran = [], res = [], capacity = 0;
-  for (let i=0; i<arr.length; i++)
-    arr[i].push(i+1);
+  for (let i = 0; i < arr.length; i++)
+    arr[i].push(i + 1);
 
   for (let item of arr)
     if (item[0] == 1)
-      kayak.push(item); 
-    else 
+      kayak.push(item);
+    else
       catamaran.push(item);
-    
-  let f = (a,b) => b[1] - a[1]; 
-  kayak.sort(f); 
+
+  let f = (a, b) => b[1] - a[1];
+  kayak.sort(f);
   catamaran.sort(f);
 
-  let i=0, j=0;
+  let i = 0, j = 0;
   if (V % 2 == 1 && kayak.length > 0) {
     capacity += kayak[0][1];
     res.push(kayak[0][2]);
@@ -43,9 +43,9 @@ function solve(arr, V) {
 
   while (V >= 2 && (i < kayak.length - 1 || j < catamaran.length)) {
     V -= 2;
-    let kayakWeight = i >= kayak.length - 1 ? -Infinity : kayak[i][1] + kayak[i+1][1];
+    let kayakWeight = i >= kayak.length - 1 ? -Infinity : kayak[i][1] + kayak[i + 1][1];
     let catamaranWeight = j == catamaran.length ? -Infinity : catamaran[j][1];
-    
+
     if (kayakWeight <= catamaranWeight) {
       capacity += catamaranWeight;
       res.push(catamaran[j][2]);
@@ -53,7 +53,7 @@ function solve(arr, V) {
     } else {
       capacity += kayakWeight;
       res.push(kayak[i][2]);
-      res.push(kayak[i+1][2]);
+      res.push(kayak[i + 1][2]);
       i += 2;
     }
   }
